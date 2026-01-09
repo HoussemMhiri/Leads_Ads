6b59abd53802d3b56c93be9df7d2b0ee9573b706
HoussemMhiri <houssemmhiri95@gmail.com>
2026-01-09 18:52:24 +0100

    feat: integrate Tailwind CSS and update App.vue
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Created a main.css file to include Tailwind's base styles and custom theme variables.
    - Updated App.vue to utilize Tailwind classes for styling, including a button and text styles.
    - Imported main.css in main.ts to ensure styles are applied globally.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <h1>You did it!</h1>
+    <button class="bg-red-500 hover:bg-error-hover">click Me</button>
+    <p class="text-3xl text-primary">
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </div>
 </template>
 
 <style scoped></style>
